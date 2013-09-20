4eb1fcc2b6b0bd528f5f6d6fdc01950c247fe3f8
root <root@pulkitpc.(none)>
2013-09-20 19:15:49 +0530

    Week1 chart.c completed

--- week1/chart.c
+++ week1/chart.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	int mm,mf,fm,ff,sum;
+	int mm,mf,fm,ff,sum,mm_sum,mf_sum,fm_sum,ff_sum;
 	float counter,i;
 	printf("Enter number of male spotting male     :");
 	scanf("%d",&mm);
@@ -14,10 +14,46 @@ int main()
 	sum = mm+mf+fm+ff;
 	counter =  80.0/sum;
 	i = 0 ;
-	while(i<sum)
+	printf("M spotting M:\n");
+	mm_sum = mm * counter ; 
+	while(i < mm_sum)
 	{
-		printf("%f\n",i);
-		i = i+counter;
+		printf("#");
+		i = i+1;
 	}
 	printf("\n");
+
+	i = 0 ;
+	printf("M spotting F:\n");
+	mf_sum = mf * counter ; 
+	while(i < mf_sum)
+	{
+		printf("#");
+		i = i+1;
+	}
+	printf("\n");
+
+
+	i = 0 ;
+	printf("F spotting M:\n");
+	fm_sum = fm * counter ; 
+	while(i < fm_sum)
+	{
+		printf("#");
+		i = i+1;
+	}
+	printf("\n");
+
+
+	i = 0 ;
+	printf("F spotting F:\n");
+	ff_sum = ff * counter ; 
+	while(i < ff_sum)
+	{
+		printf("#");
+		i = i+1;
+	}
+	printf("\n");
+
+
 }
